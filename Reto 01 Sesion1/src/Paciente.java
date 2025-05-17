665217be48adc7f12b84ee0f4af025b6419dbbbd
JaquiHdz09 <jaqueline.h6173748@gmail.com>
2025-05-16 19:19:38 -0600

    Sesion1 Reto 01

---
+++ Reto 01 Sesion1/src/Paciente.java
@@ -0,0 +1,19 @@
+public class Paciente {
+
+    private String nombre;
+    private int edad;
+    private  String expediente;
+
+    public Paciente(String nombre, int edad, String expediente) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.expediente = expediente;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Paciente: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Expediente: " + expediente);
+    }
+
+}
